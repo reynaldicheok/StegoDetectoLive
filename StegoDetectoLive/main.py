514291e6869c8ab3849aea6f286a047653858a19
LJQ <leejunquan7@gmail.com>
2024-11-10 00:14:03 +0800

    test with proxy

--- StegoDetectoLive/main.py
+++ StegoDetectoLive/main.py
@@ -1,10 +1,6 @@
 import os
 
 def main():
-    # If the images.db file does not exist, generate it
-    if not os.path.exists("images.db"):
-        os.system("python database_generation.py")
-
     # The command to start the mitmproxy server
     command = "mitmdump -s steganalysis_addon.py"
 
--- StegoDetectoLive/steganalysis_addon.py
+++ StegoDetectoLive/steganalysis_addon.py
@@ -1,9 +1,18 @@
 from mitmproxy import http
 
+from StegoDetectoLive.modules.histogram import histogram
+
 
 def request(flow: http.HTTPFlow) -> None:
-    flow.kill()
+    print(flow.request.content)
 
 
 def response(flow: http.HTTPFlow) -> None:
-    flow.kill()
+    isStego = histogram(flow.response.content)
+    if isStego:
+        print("Steganography detected")
+        flow.kill()
+    else:
+        print("No steganography detected")
+
+
