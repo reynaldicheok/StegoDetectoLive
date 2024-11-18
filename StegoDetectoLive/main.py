556c38b24ef8caed2604ec9891dc8c6908eacb1d
LJQ <leejunquan7@gmail.com>
2024-11-18 23:51:41 +0800

    Add more config options

--- StegoDetectoLive/main.py
+++ StegoDetectoLive/main.py
@@ -4,9 +4,18 @@ import json
 
 def main():
     parser = argparse.ArgumentParser(description='StegoDetectoLive')
+
+    # Select the mode of the proxy
     parser.add_argument('-R', type=str, nargs=1, help='Enable reverse proxy mode. Provide the IP address of the server')
     parser.add_argument('-F', action='store_true', help='Enable forward proxy mode')
+
+    # Configures the behavior
     parser.add_argument('-M', type=int, default=1, help='Minimum number of successful detections before blocking')
+    parser.add_argument('-T', action='store_true', help='This will allow all traffic to pass through the proxy. All '
+                                                        'image data will be saved to the triage folder with its '
+                                                        'respective metadata.')
+    
+    # Configures the proxy
     parser.add_argument('-P', type=int, default=8080, help='Port number to run the proxy')
 
     args = parser.parse_args()
@@ -16,11 +25,16 @@ def main():
         with open("config.json", "r") as f:
             config = json.load(f)
             config["min_detections"] = args.M
+            config["allow_all_traffic_and_save_data"] = args.T
         with open("config.json", "w") as f:
             json.dump(config, f)
     else:
         with open("config.json", "w") as f:
-            json.dump({"min_detections": args.M}, f)
+            config = {
+                "min_detections": args.M,
+                "allow_all_traffic_and_save_data": args.T
+            }
+            json.dump(config, f)
 
     if args.R and args.F:
         print("Please select only one mode")
--- StegoDetectoLive/steganalysis_addon.py
+++ StegoDetectoLive/steganalysis_addon.py
@@ -1,13 +1,17 @@
 import os
+from datetime import datetime
 
 from mitmproxy import http
 import json
+
+from StegoDetectoLive.modules.LSBAnalysis import lsb_stego_detection
 from StegoDetectoLive.modules.histogram import histogram
 
 # Load the configuration file
 with open("config.json", "r") as f:
     config = json.load(f)
     min_detections = config["min_detections"]
+    allow_all_traffic_and_save_data = config["allow_all_traffic_and_save_data"]
 
 # Create a directory to store the images
 if not os.path.exists("triage"):
@@ -18,9 +22,20 @@ def request(flow: http.HTTPFlow) -> None:
 
 
 def response(flow: http.HTTPFlow) -> None:
-    isStego = histogram(flow.response.content)
-    if isStego:
+    isStego = []
+    isStego.append(histogram(flow.response.content))
+    lsbMetadata, lsbIsStego = lsb_stego_detection(flow.response.content)
+    isStego.append(lsbIsStego)
+    detections = isStego.count(True)
+    
+    if detections >= min_detections:
         print("Steganography detected")
+        # Make a new folder in the triage directory with timestamp of current time
+        timestamp = str(datetime.datetime.now())
+        os.makedirs("triage/" + timestamp)
+        # Save the image in the new folder with the timestamp as the filename
+        with open("triage/" + timestamp + "/" + timestamp + ".png", "wb") as f:
+            f.write(flow.response.content)
         flow.kill()
     else:
         print("No steganography detected")
