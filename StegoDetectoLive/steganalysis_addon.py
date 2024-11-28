d9c633183fd10cf17c6ab17e9428e44ebb16bb53
LJQ <leejunquan7@gmail.com>
2024-11-28 17:25:16 +0800

    update to check stego

--- StegoDetectoLive/steganalysis_addon.py
+++ StegoDetectoLive/steganalysis_addon.py
@@ -17,27 +17,39 @@ with open("config.json", "r") as f:
 if not os.path.exists("triage"):
     os.makedirs("triage")
 
-def request(flow: http.HTTPFlow) -> None:
-    print(flow.request.content)
+
+# def request(flow: http.HTTPFlow) -> None:
+#     # Block all images requests
+#     if "image" in flow.request.headers[b"Content-Type"]:
+#         flow.kill()
 
 
 def response(flow: http.HTTPFlow) -> None:
-    isStego = []
-    isStego.append(histogram(flow.response.content))
-    lsbMetadata, lsbIsStego = lsb_stego_detection(flow.response.content)
-    isStego.append(lsbIsStego)
-    detections = isStego.count(True)
-    
-    if detections >= min_detections:
-        print("Steganography detected")
-        # Make a new folder in the triage directory with timestamp of current time
-        timestamp = str(datetime.datetime.now())
-        os.makedirs("triage/" + timestamp)
-        # Save the image in the new folder with the timestamp as the filename
-        with open("triage/" + timestamp + "/" + timestamp + ".png", "wb") as f:
-            f.write(flow.response.content)
-        flow.kill()
-    else:
-        print("No steganography detected")
+    # Block all images responses
+    # Check if the response headers contain "Content-Type"
+    if b"Content-Type" in flow.response.headers and "image" in flow.response.headers[b"Content-Type"]:
+        to_kill = check_Stego(flow.response.content)
+        if to_kill:
+            flow.kill()
 
 
+def check_Stego(data):
+    isStegoList = []
+    # Check if the image is steganography
+    lsbIsStego, lsbMetadata = lsb_stego_detection(data)
+    isStegoList.append(lsbIsStego)
+
+    histogramIsStego = histogram(data)
+    isStegoList.append(histogramIsStego)
+    detections = isStegoList.count(True)
+    if detections >= min_detections or allow_all_traffic_and_save_data:
+        print("Steganography image detected")
+        # Save the image to the triage folder
+        with open("triage/{}.png".format(datetime.now().strftime("%Y%m%d%H%M%S")), "wb") as f:
+            f.write(data)
+        # Save the metadata to the triage folder
+        with open("triage/{}.json".format(datetime.now().strftime("%Y%m%d%H%M%S")), "w") as f:
+            json.dump(lsbMetadata, f)
+        if not allow_all_traffic_and_save_data:
+            return True
+    return False
