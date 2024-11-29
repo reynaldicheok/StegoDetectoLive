d6f66873ad94611f54499ca1baa6ef681a3bc9a1
LJQ <leejunquan7@gmail.com>
2024-11-29 23:37:34 +0800

    Instead of using content type, check magic bytes

--- StegoDetectoLive/steganalysis_addon.py
+++ StegoDetectoLive/steganalysis_addon.py
@@ -5,6 +5,7 @@ import json
 from requests_toolbelt.multipart import decoder
 from StegoDetectoLive.modules.LSBAnalysis import lsb_stego_detection
 from StegoDetectoLive.modules.histogram import histogram
+import pyfsig
 
 # Load the configuration file
 with open("config.json", "r") as f:
@@ -16,52 +17,87 @@ with open("config.json", "r") as f:
 if not os.path.exists("triage"):
     os.makedirs("triage")
 
-
+img_types_list = ["png", "jpg", "jpeg"]
 def request(flow: http.HTTPFlow) -> None:
-    # Block all images requests
-    if b"Content-Type" in flow.request.headers and "image" in flow.request.headers[b"Content-Type"]:
-        to_kill = check_Stego(flow.request.content)
-        if to_kill:
-            flow.kill()
-
+    # For multipart/form-data requests
     if b"Content-Type" in flow.request.headers and "multipart/form-data" in flow.request.headers[b"Content-Type"]:
-        to_kill = False
-        multipart_data = decoder.MultipartDecoder(flow.request.content, flow.request.headers["Content-Type"])
-        for part in multipart_data.parts:
-            try:
-                to_kill = check_Stego(part.content)
-            except Exception as e:
-                print("Not an image")
-        if to_kill:
+        try:
+            to_kill = False
+            multipart_data = decoder.MultipartDecoder(flow.request.content, flow.request.headers["Content-Type"])
+            for part in multipart_data.parts:
+                fileBytes = part.content[:128]
+                fileTypes = pyfsig.find_matches_for_file_header(file_header=fileBytes)
+                isImage = False
+                fileExtension = None
+                for fileType in fileTypes:
+                    if fileType.file_extension in img_types_list:
+                        isImage = True
+                        fileExtension = fileType.file_extension
+                        break
+                if isImage:
+                    to_kill = check_Stego(part.content, fileExtension)
+            if to_kill:
+                flow.kill()
+        except Exception as e:
+            print("Content type is not multipart/form-data")
             flow.kill()
+    # else inspect the contents for images
+    else:
+        fileBytes = flow.request.content[:128]
+        fileTypes = pyfsig.find_matches_for_file_header(file_header=fileBytes)
+        isImage = False
+        fileExtension = None
+        for fileType in fileTypes:
+            if fileType.file_extension in img_types_list:
+                isImage = True
+                fileExtension = fileType.file_extension
+                break
+        if isImage:
+            to_kill = check_Stego(flow.request.content, fileExtension)
+            if to_kill:
+                flow.kill()
 
 
 def response(flow: http.HTTPFlow) -> None:
-    # Block all images responses
-    # Check if the response headers contain "Content-Type"
-    if b"Content-Type" in flow.response.headers and "image" in flow.response.headers[b"Content-Type"]:
-        to_kill = check_Stego(flow.response.content)
+    # inspect the contents for images
+    fileBytes = flow.response.content[:128]
+    fileTypes = pyfsig.find_matches_for_file_header(file_header=fileBytes)
+    isImage = False
+    fileExtension = None
+    for fileType in fileTypes:
+        if fileType.file_extension in img_types_list:
+            isImage = True
+            break
+
+    if isImage:
+        to_kill = check_Stego(flow.response.content, fileExtension)
         if to_kill:
             flow.kill()
+            return
 
 
-def check_Stego(data):
+def check_Stego(data, fileExtension):
     isStegoList = []
+    stegoMetadata = {}
     # Check if the image is steganography
+    # LSB
     lsbIsStego, lsbMetadata = lsb_stego_detection(data)
     isStegoList.append(lsbIsStego)
+    stegoMetadata["LSB"] = lsbMetadata
 
+    # Histogram
     histogramIsStego = histogram(data)
     isStegoList.append(histogramIsStego)
+
     detections = isStegoList.count(True)
     if detections >= min_detections or allow_all_traffic_and_save_data:
         print("Steganography image detected")
         # Save the image to the triage folder
-        with open("triage/{}.png".format(datetime.now().strftime("%Y%m%d%H%M%S")), "wb") as f:
+        with open("triage/{}.{}".format(datetime.now().strftime("%Y%m%d%H%M%S"), fileExtension), "wb") as f:
             f.write(data)
         # Save the metadata to the triage folder
         with open("triage/{}.json".format(datetime.now().strftime("%Y%m%d%H%M%S")), "w") as f:
-            json.dump(lsbMetadata, f)
+            json.dump(stegoMetadata, f)
         if not allow_all_traffic_and_save_data:
             return True
     return False
